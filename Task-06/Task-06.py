634c2078b808ef540edf700bbb29015f932795d8
ProgmanZ <dumphill@gmail.com>
2022-09-17 19:44:20 +0300

    Task-06 Готово

--- Task-06/Task-06.py
+++ Task-06/Task-06.py
@@ -1,2 +1,22 @@
 # Задача 6. Паранойя
 
+file_input = open('text.txt', 'r')
+file_output = open('cipher.txt', 'w')
+
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUWVXYZabcdefghijklmnopqrstuwvxyz'
+
+count = 0
+
+for line in file_input:
+    new_line = str()
+    count += 1
+    for letter in line:
+        if letter in alphabet:
+            new_line += alphabet[alphabet.index(letter)+count]
+        else:
+            new_line += letter
+    file_output.write(new_line)
+
+
+file_input.close()
+file_output.close()
